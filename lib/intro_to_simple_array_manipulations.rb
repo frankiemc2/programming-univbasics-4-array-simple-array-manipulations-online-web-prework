334a3ecc81d7937c027869d322c98f6146de5040
francisco cabrera <fjcabrera@buffalo.edu>
2019-10-05 02:24:56 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+def using_push(array,string)
+colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+next_color = "violet"
+colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(array,string)
+    bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+    new_neighborhood = "Staten Island"
+    bouroughs_in_nyc.unshift(new_neighborhood)
+  end
+
+def using_pop(array)
+
+continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+deleted_string = continents.pop
+end
+
+def pop_with_args(array)
+    dog_breeds = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  dog_breeds.pop= "Chihuahua", "Shiba Inu"
+end
