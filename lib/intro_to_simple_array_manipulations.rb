feb44acd65b12faed61aa81fa26ea532c8ae6c21
francisco cabrera <fjcabrera@buffalo.edu>
2019-10-05 03:19:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -33,7 +33,7 @@ def using_concat(array1,array2)
 array1.concat(array2)
 end
 
-def using_insert(array1,element)
-  array1.insert(element)
+def using_insert(array,element)
+  array.insert(0,element)
 
 end
